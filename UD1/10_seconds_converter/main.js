1bbadf3d741acc9bb87005df7fa5fb5cc9bb1c09
takervare <takervare@gmail.com>
2025-09-22 17:15:21 +0200

    Subo archivos que no había subido

--- UD1/10_seconds_converter/main.js
+++ UD1/10_seconds_converter/main.js
@@ -1,7 +1,24 @@
 const assert = require('assert').strict;
 
 function toHoursMinutesSeconds(value) {
+    let hours = 0
+    let minuts =0
+    let seconds =0
+
+    if(value>= 3600){
+        hours=parseInt(value/3600)
+        value=value%3600
+    }
+    if(value>=60){
+        minuts=parseInt(value/60)
+        value=value%60
+    }
+    seconds=value
+
+    return `${hours}:${minuts}:${seconds}`
 }
+console.log(toHoursMinutesSeconds(3600));
+
 
 assert.deepStrictEqual(toHoursMinutesSeconds(3600), "1:0:0")
 assert.deepStrictEqual(toHoursMinutesSeconds(3720), "1:20:0")
