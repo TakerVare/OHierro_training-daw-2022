2704a0e8d20fb18fc29ea1019574a582acd34841
takervare <takervare@gmail.com>
2025-09-22 20:30:25 +0200

    Ejercicio UD1 Ejercicio 04

--- UD1/04_calculator/main.js
+++ UD1/04_calculator/main.js
@@ -8,6 +8,27 @@
 // define variables
 let x, y;
 
+x=10
+y=5
+
+function sum(x, y){
+    return (x + y)
+}
+
+function dif(x, y){
+    return (x-y)
+}
+
+function product(x, y){
+    return (x*y)
+}
+
+function div(x, y){
+    return (x/y)
+}
 
 // print sum, substract, multiply and divide
-console.log('something')
+console.log(`El resutado de la suma de ${x} y ${y} es = ${sum(x, y)}`)
+console.log(`El resutado de la resta de ${x} y ${y} es = ${dif(x, y)}`)
+console.log(`El resutado de la multiplicación de ${x} y ${y} es = ${product(x, y)}`)
+console.log(`El resutado de la división de ${x} y ${y} es = ${div(x, y)}`)
