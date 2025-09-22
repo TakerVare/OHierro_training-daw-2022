a63274d7ba8db2116d7e45eb850d5cb068039745
takervare <takervare@gmail.com>
2025-09-22 16:37:14 +0200

    Ejercicio resulto sin assert

--- UD1/08_abs/main.js
+++ UD1/08_abs/main.js
@@ -1,8 +1,21 @@
+const { log } = require('console');
+
 const assert = require('assert').strict;
 
 function abs(value) {
+    let result=value;
+    if(value<0){
+        result=value*(-1);
+    }
 
+    return result;
 }
+
+
+console.log(`El valor absoluto de 4 es ${abs(4)}`);
+console.log(`El valor absoluto de -4 es ${abs(4)}`);
+console.log(`El valor absoluto de 0 es ${abs(0)}`);
+
 
 assert.strictEqual(abs(4), 4)
 assert.strictEqual(abs(-4), 4)
