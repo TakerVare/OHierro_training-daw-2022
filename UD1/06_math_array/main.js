af4281c8954138351fcf0c8f7f902a8adcd37919
takervare <takervare@gmail.com>
2025-09-22 16:16:06 +0200

    Moda

--- UD1/06_math_array/main.js
+++ UD1/06_math_array/main.js
@@ -23,12 +23,47 @@ posición específica del arreglo.
 ```javascript
 let array = [1, 2, 3];
 array.splice(1, 0, 4); // [1, 4, 2, 3]
+
+*Métodos para extraer elementos**
+
+1. **`shift()`**: Elimina el primer elemento del 
+arreglo y lo devuelve.
+```javascript
+let array = [1, 2, 3];
+let elemento = array.shift(); // 1
+console.log(array); // [2, 3]
+```
+2. **`pop()`**: Elimina el último elemento del arreglo 
+y lo devuelve.
+```javascript
+let array = [1, 2, 3];
+let elemento = array.pop(); // 3
+console.log(array); // [1, 2]
+```
+3. **`slice()`**: Extrae una parte del arreglo y la 
+devuelve como un nuevo arreglo.
+```javascript
+let array = [1, 2, 3, 4, 5];
+let subArray = array.slice(1, 3); // [2, 3]
+console.log(array); // [1, 2, 3, 4, 5]
+```
+4. **`splice()`**: Puedes utilizar `splice()` para 
+extraer elementos del arreglo y reemplazarlos con 
+otros.
+```javascript
+let array = [1, 2, 3];
+let elemento = array.splice(1, 1); // [2]
+console.log(array); // [1, 3]
+
 */
 
 let array= [1,2,3,4];
 
 
 function doCalculation(array) {
+    //Imprimo el array
+    console.log(array);
+
     //Imprimmo la suma
     console.log(`La suma del array es: ${getSum(array)}`);
 
@@ -40,6 +75,14 @@ function doCalculation(array) {
 
     //Imprimo la media
     console.log(`La media del array es: ${getAverage(array)}`);
+
+    //Imprimo la moda
+    console.log(`La moda del array es: ${getMode(array)}`);
+
+    //Imprimo separación
+    console.log(`##############################################`);
+    
+    
 }
 
 function getSum(array) {
@@ -82,6 +125,20 @@ function getAverage(array) {
 
 function getMode(array){
     let frequency = {};
+    let maxFreq = 0;
+    let mode = null;
+
+    for (let i = 0; i < array.length; i++) {
+        let num = array[i];
+        frequency[num] = (frequency[num] || 0) + 1;
+
+        if (frequency[num] > maxFreq) {
+            maxFreq = frequency[num];
+            mode = num;
+        }
+    }
+    return mode;
+
 }
 
 
